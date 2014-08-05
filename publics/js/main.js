f7adc0bc27846b3a3f121e6c49f133111974887c
creeperyang <yangcreeper@hotmail.com>
2014-08-05 17:55:11 +0800

    index page 60%

--- publics/js/main.js
+++ publics/js/main.js
@@ -1,3 +1,20 @@
 $(function(){
     var cyDropdown = new $.CyDropdown($(".cy-dropdown")[0]);
+
+    // page scroll
+    var $window = $(window),
+        $headerPage = $("#user-info"),
+        $desPage = $("#des-page"),
+        windowHeight = $window.outerHeight(),
+        headerPageHeight = $headerPage.outerHeight(),
+        desPageHeight = $desPage.height();
+    console.log(windowHeight, headerPageHeight, desPageHeight);
+    /*var delta = windowHeight - headerPageHeight - desPageHeight;
+    var paddingTop = $headerPage.css("padding-top");
+    if(delta > 0) {
+        console.log(delta, paddingTop)
+        $headerPage.css({
+            "padding-top": delta + parseInt(paddingTop) + "px"
+        });
+    }*/
 });
