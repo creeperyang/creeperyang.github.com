04010343f52ad6c279e48f1316f4a78ffdf22439
creeperyang <yangcreeper@hotmail.com>
2014-12-24 00:45:12 +0800

    add link page

--- publics/scripts/scrollToTop.js
+++ publics/scripts/scrollToTop.js
@@ -9,22 +9,16 @@ $(function() {
             $backToTop.hide();
         }
     };
+    
     toggleSideMenu();
+
     $window.scroll(function() {
         toggleSideMenu();
     });
+
     $backToTop.on('click', function() {
         $('html, body').animate({
             scrollTop: 0
         }, 1000);
-    });
-
-    // wheel menu
-    $(".wheel-button").wheelMenu({
-        trigger: "click", // Can be "click" or "hover". Default: "click"
-        animationSpeed: "fast", // Entrance animation speed. Can be "instant", "fast", "medium", or "slow". Default: "medium"
-        angle: [200, 340],
-        width: 120,
-        height: 120
     });
 });
